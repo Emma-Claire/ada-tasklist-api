cbc6419a3814b3edaa0f7081b857d4d3186f264a
Dan Roberts <dan@adadevelopersacademy.org>
2016-12-18 15:58:20 -0800

    Add controller methods for create, update and destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,11 +16,44 @@ class TasksController < ApplicationController
   end
 
   def create
+    task = Task.new(task_params)
+    if task.save
+      render status: :created, json: {id: task.id}
+    else
+      render status: :bad_request, json: {
+        errors: task.errors.messages
+      }
+    end
   end
 
   def update
+    begin
+      task = Task.find(params[:id])
+      task.assign_attributes(task_params)
+      if task.save
+        render status: :no_content, content: false
+      else
+        render status: :bad_request, json: {
+          errors: task.errors.messages
+        }
+      end
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
   def destroy
+    begin
+      task = Task.find(params[:id])
+      task.destroy
+      render status: :no_content, content: false
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
+  end
+
+private
+  def task_params
+    params.require(:task).permit(:title, :description, :completed)
   end
 end

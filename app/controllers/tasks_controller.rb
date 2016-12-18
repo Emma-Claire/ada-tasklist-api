d7723c5a242bacc692259690b4afed41cae40b7f
Dan Roberts <dan@adadevelopersacademy.org>
2016-12-18 15:22:20 -0800

    Get tasks list working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,20 @@
 class TasksController < ApplicationController
+  def index
+    render json: {
+      count: Task.count,
+      tasks: Task.all.as_json(only: [:id, :title, :description, :completed])
+    }
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end

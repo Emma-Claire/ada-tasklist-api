e853926e8c436f347995ba2a0d219dc4fddfc3fb
Dan Roberts <dan@adadevelopersacademy.org>
2016-12-18 20:00:31 -0800

    Add CORS preflight options route for /tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match "/tasks", controller: 'application', action: 'cors_preflight_check', via: [:options]
   match "/tasks/:id", controller: 'application', action: 'cors_preflight_check', via: [:options]
   resources :tasks, except: [:new, :edit]
 end
